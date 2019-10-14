d9b649dd1c5d4aeb2fc51c88d3da9c34f2f3a175
Tom Mason <tommason14@gmail.com>
2019-10-14 23:46:05 +1100

    Now also prints file and bonds

--- bond_lengths.sh
+++ bond_lengths.sh
@@ -12,5 +12,5 @@ elif [[ $1 == '-h' || $1 == '--help' ]] ; then
   echo 'Then run this script with the two atoms to look for'
   echo 'i.e. bond_lengths.sh C N'
 else
-  cat bonds.txt | grep -E '^[[:lower:]]|^\s*'$1'[0-9]*\s*'$2'[0-9]*|\s*'$2'[0-9]*\s*'$1'[0-9]*'
+  cat bonds.txt | grep -E '^[[:lower:]]|^\s*'$1'[0-9]*\s*'$2'[0-9]*|\s*'$2'[0-9]*\s*'$1'[0-9]*|Using'
 fi
