9e402a125c1544faa4990c81688d12a68cbd3b4a
tommason14 <tom.mason14@gmail.com>
2019-10-28 21:11:43 +1100

    fixed

--- combine_csvs.sh
+++ combine_csvs.sh
@@ -1,1 +1,17 @@
-cat $@ | sed '2,${/File/d;}'
+#!/usr/bin/env sh
+
+# help="Pass in csv files that you would like to combine.\n"
+# help+="i.e. combine_csvs file1 file2\n"
+# help+="Note: Assumes that header lines include the word 'File'\n"
+#
+# if grep -eq "-h" "$@"
+# then 
+# echo $help
+# exit 
+# fi
+
+cat $@ | sed '2,${/File/d;}' > tmp_file
+header=$(head -n 1 tmp_file)
+echo $header
+cat tmp_file | tail -n +2 | sort -t ',' -k 1
+rm tmp_file
