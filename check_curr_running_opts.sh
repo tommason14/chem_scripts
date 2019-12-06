9b338754bfbd7b50acb4242fe84d902ad0c3de81
tommason14 <tom.mason14@gmail.com>
2019-12-06 19:31:20 +1100

    removed && ||, moved to if else

--- check_curr_running_opts.sh
+++ check_curr_running_opts.sh
@@ -6,8 +6,15 @@ for f in $(squeue -u $USER | grep $USER | awk '{print $1}')
 do 
 cd $(scontrol show jobid $f | grep WorkDir | awk -F '=' '{print $2}')
 pwd
-[[ -f opt-freq.log ]] && (cat opt-freq.log | plotgauss) || (
-grep -iq 'FMO' opt.log && (cat opt.log | plotfmo) || (
-cat opt.log | plotmp2) 
-)
+if [[ -f opt-freq.log ]] 
+then 
+  cat opt-freq.log | plotgauss
+else
+  if grep -iq 'FMO' opt.log 
+  then
+  cat opt.log | plotfmo
+  else
+  cat opt.log | plotmp2) 
+  fi
+fi
 done
