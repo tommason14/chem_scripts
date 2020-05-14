1384dbd6d31b883331206ba1a03208388c0c1975
tommason14 <tom.mason14@gmail.com>
2020-05-14 22:36:09 +1000

    travis cluster and cut

--- after-finding-ts-gaussian.py
+++ after-finding-ts-gaussian.py
@@ -2,35 +2,33 @@ from autochem import Settings, GaussJob
 from glob import glob
 import os
 
-xyz = glob('*xyz')[0]
+xyz = glob("*xyz")[0]
 
-gas_free=Settings()
+gas_free = Settings()
 gas_free.input.freq = True
-gas_free.input.method='M052X'
-gas_free.input.basis='6-311+g(d,p)'
-gas_free.meta.time='12:00:00'
-gas_free.meta.mem='96gb'
-gas_free.meta.nproc='24'
+gas_free.input.method = "M052X"
+gas_free.input.basis = "6-311+g(d,p)"
+gas_free.meta.time = "12:00:00"
+gas_free.meta.mem = "96gb"
+gas_free.meta.nproc = "24"
 
-solv_free=Settings()
+solv_free = Settings()
 solv_free.input.freq = True
-solv_free.input.method='M052X'
-solv_free.input.basis='6-311+g(d,p)'
-solv_free.input.scrf='smd,solvent=water'
-solv_free.meta.time='12:00:00'
-solv_free.meta.mem='96gb'
-solv_free.meta.nproc='24'
+solv_free.input.method = "M052X"
+solv_free.input.basis = "6-311+g(d,p)"
+solv_free.input.scrf = "smd,solvent=water"
+solv_free.meta.time = "12:00:00"
+solv_free.meta.mem = "96gb"
+solv_free.meta.nproc = "24"
 
-gas_spec=Settings()
-gas_spec.input.method='wB97XD'
-gas_spec.input.basis='aug-cc-pVTZ'
-gas_spec.meta.time='12:00:00'
-gas_spec.meta.mem='96gb'
-gas_spec.meta.nproc='32'
+gas_spec = Settings()
+gas_spec.input.method = "wB97XD"
+gas_spec.input.basis = "aug-cc-pVTZ"
+gas_spec.meta.time = "12:00:00"
+gas_spec.meta.mem = "96gb"
+gas_spec.meta.nproc = "32"
 
-jobs = {'gas_free': gas_free,
-        'solv_free': solv_free,
-        'gas_spec': gas_spec}
+jobs = {"gas_free": gas_free, "solv_free": solv_free, "gas_spec": gas_spec}
 
 # Create separate dirs and make jobs
 
@@ -39,5 +37,5 @@ for newdir, settings_file in jobs.items():
     if not os.path.isdir(newdir):
         os.mkdir(newdir)
     os.chdir(newdir)
-    GaussJob(f'../{xyz}', settings=settings_file)
+    GaussJob(f"../{xyz}", settings=settings_file)
     os.chdir(cwd)
---
+++ travis_split_cluster_into_steps.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit(f'Syntax: {os.path.basename(__file__)} <cluster.xyz>')
+
+files = {}
+
+with open(sys.argv[1]) as f:
+    steps = [re.search('Step ([0-9]+)', line).group(1)
+             for line in f if '#' in line]
+
+clusters = []
+tmp = []
+with open(sys.argv[1]) as f:
+    for line in f:
+        if re.search('^\s*[0-9]+$', line) and len(tmp) != 0:
+            clusters.append(tmp)
+            tmp = []
+        tmp.append(line)  
+            
+if not os.path.isdir('clusters'):
+    os.mkdir('clusters')
+
+for step, cluster in zip(steps, clusters):
+    with open(f"clusters/step-{step}.xyz", 'w') as new:
+        for line in cluster:
+            new.write(line)
